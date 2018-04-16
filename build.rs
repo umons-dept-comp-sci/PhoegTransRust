e5ee314cf77fc56fb9783ade3e4fd58516638574
Gauvain Devillez <gauvain.devillez@umons.ac.be>
2018-04-16 11:05:31 +0200

    Removed build.rs

--- build.rs
+++
@@ -1,6 +0,0 @@
-use std::process::Command;
-
-fn main() {
-    Command::new("make").current_dir("../graph/nauty-wrapper").output().expect("Failed to build");
-    println!("cargo:rustc-link-search=../graph/nauty-wrapper")
-}
